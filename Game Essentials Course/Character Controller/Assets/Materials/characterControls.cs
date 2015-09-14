de87a95966f28c1759b851dbae9773eb437f318a
jorey-dixon <greendraken@gmail.com>
2015-09-14 01:25:26 -0600

    Added Assignment to make things move
    Assignment to make a block move, with a spring to add a little flavor.

---
+++ Game Essentials Course/Character Controller/Assets/Materials/characterControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterControls : MonoBehaviour {
+			
+		public float speed = 5;
+		private Rigidbody rb;
+		
+		void FixedUpdate () 
+	
+		{
+			rb = GetComponent<Rigidbody> ();
+
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0);
+
+		if (moveHorizontal >= 6)
+		{
+			moveHorizontal = 6;
+		}
+		if (moveVertical >= 6)
+		{
+			moveVertical = 6;
+		}
+
+		rb.AddForce (movement * speed);
+			
+		}
+	}
+
